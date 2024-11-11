c5b9a45a78ac727fe54bdffb990c2a1756bb5a97
Adrian Brown <ninobrown58518@gmail.com>
2024-11-11 12:34:02 -0600

    puppy pals wrap up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+
+import { useState } from 'react'
 import './App.css'
-// import puppyList from './puppyList.json';
+import {puppyList} from './data.js';
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  // function handleClick() {
+  //   // some logic here
+  // }
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
 
   return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App
